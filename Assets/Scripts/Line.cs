3cb655ce444b9ba1d9556ac3b6f3910d97b1d176
aroussiteyeb <aroussi199@gmail.com>
2022-11-19 14:17:57 +0100

    aroussi

--- Assets/Scripts/Line.cs
+++ Assets/Scripts/Line.cs
@@ -8,7 +8,7 @@ public class Line : MonoBehaviour {
 	public Rigidbody2D rigidBody;
 
 	[HideInInspector] public List<Vector2> points = new List<Vector2> ( );
-	[HideInInspector] public int pointsCount = 0;
+	[HideInInspector] public int pointsCounts = 0;
 
 	//The minimum distance between line's points.
 	float pointsMinDistance = 0.1f;
@@ -18,11 +18,11 @@ public class Line : MonoBehaviour {
 
 	public void AddPoint ( Vector2 newPoint ) {
 		//If distance between last point and new point is less than pointsMinDistance do nothing (return)
-		if ( pointsCount >= 1 && Vector2.Distance ( newPoint, GetLastPoint ( ) ) < pointsMinDistance )
+		if ( pointsCounts >= 1 && Vector2.Distance ( newPoint, GetLastPoint ( ) ) < pointsMinDistance )
 			return;
 
 		points.Add ( newPoint );
-		pointsCount++;
+		pointsCounts++;
 
 		//Add Circle Collider to the Point
 		CircleCollider2D circleCollider = this.gameObject.AddComponent <CircleCollider2D> ( );
@@ -30,17 +30,17 @@ public class Line : MonoBehaviour {
 		circleCollider.radius = circleColliderRadius;
 
 		//Line Renderer
-		lineRenderer.positionCount = pointsCount;
-		lineRenderer.SetPosition ( pointsCount - 1, newPoint );
+		lineRenderer.positionCount = pointsCounts;
+		lineRenderer.SetPosition ( pointsCounts - 1, newPoint );
 
 		//Edge Collider
 		//Edge colliders accept only 2 points or more (we can't create an edge with one point :D )
-		if ( pointsCount > 1 )
+		if ( pointsCounts > 1 )
 			edgeCollider.points = points.ToArray ( );
 	}
 
 	public Vector2 GetLastPoint ( ) {
-		return ( Vector2 )lineRenderer.GetPosition ( pointsCount - 1 );
+		return ( Vector2 )lineRenderer.GetPosition ( pointsCounts - 1 );
 	}
 
 	public void UsePhysics ( bool usePhysics ) {
